aeda32a0e42616b869f0f83aa9abeab92595b1d6
Kimberly <kbrown1@unc.edu>
2021-09-07 19:17:38 -0400

    count_letters changes

--- exercises/ex02/count_letters.py
+++ exercises/ex02/count_letters.py
@@ -2,5 +2,18 @@
 
 __author__ = "730481343"
 
+search_letter: str = input("What letter do you want to search for?: " )
+search_word: str = input("Enter a word: ")
 
-# Begin your solution here...
+counter: int = 0
+search_result: int = 0
+
+while counter < len(search_word): 
+    if search_word[counter] == search_letter: 
+        search_result = search_result + 1
+        counter = counter + 1
+    else: 
+        counter = counter + 1
+
+search_result_string: str = str(search_result)
+print("Count: " + search_result_string)
