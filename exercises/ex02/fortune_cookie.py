bf9cf34568800883541e3c6167d732e1611b09db
Kimberly <kbrown1@unc.edu>
2021-09-07 11:28:09 -0400

    ex02 continuation

--- exercises/ex02/fortune_cookie.py
+++ exercises/ex02/fortune_cookie.py
@@ -6,15 +6,15 @@ from random import randint
 
 print("Your fortune cookie says...")
 
-randomNum: int = randint(1,4)
+random_number: int = randint(1,4)
 
-if randomNum < 3:  
-    if randomNum == 1: 
+if random_number < 3:  
+    if random_number == 1: 
         print("A foolish man listens to his heart. A wise man listens to cookies.")
     else: 
         print("To truly find yourself you should play hide and seek alone.")
 else: 
-    if randomNum == 3: 
+    if random_number == 3: 
         print("You are about to finish reading a fortune cookie.")
     else: 
         print("So live that you wouldn't be ashamed to sell the family parrot to the town gossip")
--- exercises/ex02/repeat_beat.py
+++ exercises/ex02/repeat_beat.py
@@ -1,6 +1,24 @@
 """Repeating a beat in a loop."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730481343"
 
+beat: str = input("What beat do you want to repeat? ")
 
-# Begin your solution here...
+repeat_times: int = int(input("How many times do you want to repeat it? "))
+
+counter: int = 0
+
+if repeat_times < 0: 
+    print("No beat...")
+
+else: 
+    while counter < repeat_times: 
+        while repeat_times != counter:
+            repeat_beats: str = beat
+            final_beats: str = repeat_beats + "5"
+            counter = counter + 1
+            print(final_beats)
+        
+        # final_beat = final_beats
+        counter = counter + 1
+        
