57c93c6f6d8c449cd596a6a3feb745adc0fedc52
Kimberly <kbrown1@unc.edu>
2021-09-11 13:22:01 -0400

    tree image program complete

--- exercises/ex03/happy_trees.py
+++ exercises/ex03/happy_trees.py
@@ -1,6 +1,22 @@
 """Drawing forests in a loop."""
 
-__author__ = "123456789"
+__author__ = "730481343"
 
 # The string constant for the pine tree emoji
 TREE: str = '\U0001F332'
+
+depth: int = int(input("Depth: "))
+counter: int = 0
+
+tree_image: str = ""
+
+if depth > 0: 
+    while counter < depth: 
+        if counter != depth:
+            tree_image = tree_image + TREE + " "
+            counter = counter + 1
+            print(tree_image)
+        else: 
+            beat_string = tree_image + TREE
+            counter = counter + 1
+            print(tree_image)
